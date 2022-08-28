9f24ca2a6969ab1c54c14bb4852ae4d6c83cbfc7
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2022-08-28 17:10:38 -0300

    signext

--- signext.sv
+++ signext.sv
@@ -1,5 +1,11 @@
 module signext(input logic [31:0] a, output logic [63:0] b);
-
-
-
+	always_comb
+		case(a[31:21])
+			11'b111_1100_0010, // LDUR
+				11'b111_1100_0000: // STUR
+					b = {{55{a[21]}}, a[20:12]};
+			11'b101_1010_0???: // CBZ
+					b = {{45{a[23]}}, a[23:5]};
+			default: b = 0;
+		endcase
 endmodule
