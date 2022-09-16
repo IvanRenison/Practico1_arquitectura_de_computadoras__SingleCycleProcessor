2380aee461bf73d5219233fc232a991ff5c436fd
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2022-09-16 10:06:19 -0300

    Actualizado maindec

--- maindec.sv
+++ maindec.sv
@@ -1,60 +1,95 @@
 module maindec(
 		input logic [10:0] Op,
-		output logic Reg2Loc, ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch,
-		output logic [1:0] ALUOp
+		output logic Reg2Loc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, ERet,
+		output logic [1:0] ALUSrc, ALUOp,
+		output logic [3:0] EStatus
 	);
 
 	always_comb begin
 		casez(Op)
 		11'b111_1100_0010: begin // LDUR
 				Reg2Loc <= 1'b0;
-				ALUSrc <= 1'b1;
 				MemtoReg <= 1'b1;
 				RegWrite <= 1'b1;
 				MemRead <= 1'b1;
 				MemWrite <= 1'b0;
 				Branch <= 1'b0;
+				ERet <= 1'b0;
+				ALUSrc <= 2'b01;
 				ALUOp <= 2'b00;
+				EStatus <= 4'b0000;
 			end
 		11'b111_1100_0000: begin  // STUR
 				Reg2Loc <= 1'b1;
-				ALUSrc <= 1'b1;
 				MemtoReg <= 1'b0;
 				RegWrite <= 1'b0;
 				MemRead <= 1'b0;
 				MemWrite <= 1'b1;
 				Branch <= 1'b0;
+				ERet <= 1'b0;
+				ALUSrc <= 2'b01;
 				ALUOp <= 2'b00;
+				EStatus <= 4'b0000;
 			end
 		11'b101_1010_0???: begin  // CBZ
 				Reg2Loc <= 1'b1;
-				ALUSrc <= 1'b0;
 				MemtoReg <= 1'b0;
 				RegWrite <= 1'b0;
 				MemRead <= 1'b0;
 				MemWrite <= 1'b0;
 				Branch <= 1'b1;
+				ERet <= 1'b0;
+				ALUSrc <= 2'b00;
 				ALUOp <= 2'b01;
+				EStatus <= 4'b0000;
 			end
 		11'b100_0101_1000, 11'b110_0101_1000, 11'b100_0101_0000, 11'b101_0101_0000: begin // ADD, SUB, AND, ORR (tipo R)
 				Reg2Loc <= 1'b0;
-				ALUSrc <= 1'b0;
 				MemtoReg <= 1'b0;
 				RegWrite <= 1'b1;
 				MemRead <= 1'b0;
 				MemWrite <= 1'b0;
 				Branch <= 1'b0;
+				ERet <= 1'b0;
+				ALUSrc <= 2'b00;
 				ALUOp <= 2'b10;
+				EStatus <= 4'b0000;
+			end
+		11'b110_1011_0100: begin  // ERET
+				Reg2Loc <= 1'b0;
+				MemtoReg <= 1'b0;
+				RegWrite <= 1'b0;
+				MemRead <= 1'b0;
+				MemWrite <= 1'b0;
+				Branch <= 1'b1;
+				ERet <= 1'b1;
+				ALUSrc <= 2'b00;
+				ALUOp <= 2'b01;
+				EStatus <= 4'b0000;
+			end
+		11'b110_1010_1001: begin  // MRS
+				Reg2Loc <= 1'b1;
+				MemtoReg <= 1'b0;
+				RegWrite <= 1'b1;
+				MemRead <= 1'b0;
+				MemWrite <= 1'b0;
+				Branch <= 1'b0;
+				ERet <= 1'b0;
+				ALUSrc <= 2'b10;
+				ALUOp <= 2'b01;
+				EStatus <= 4'b0000;
 			end
 		default: begin
 				Reg2Loc <= 1'b0;
-				ALUSrc <= 1'b0;
 				MemtoReg <= 1'b0;
 				RegWrite <= 1'b0;
 				MemRead <= 1'b0;
 				MemWrite <= 1'b0;
 				Branch <= 1'b0;
+				ERet <= 1'b0;
+				ALUSrc <= 2'b00;
 				ALUOp <= 2'b00;
+				EStatus <= 4'b0000;
 			end
 		endcase
 	end
--- maindec_tb.sv
+++ maindec_tb.sv
@@ -2,27 +2,31 @@ module maindec_tb();
 
 	logic [10:0] opcode;
 
-	logic Reg2Loc, ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch;
-	logic [1:0] ALUOp;
+	logic Reg2Loc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, ERet;
+	logic [1:0] ALUOp, ALUSrc;
+	logic [3:0] EStatus;
 
 	maindec dut(
 		.Op(opcode),
 		.Reg2Loc(Reg2Loc), .ALUSrc(ALUSrc), .MemtoReg(MemtoReg), .RegWrite(RegWrite),
-		.MemRead(MemRead), .MemWrite(MemWrite), .Branch(Branch), .ALUOp(ALUOp)
+		.MemRead(MemRead), .MemWrite(MemWrite), .Branch(Branch), .ALUOp(ALUOp), .ERet(ERet),
+		.EStatus(EStatus)
 	);
 
-	logic expected_Reg2Loc, expected_ALUSrc, expected_MemtoReg, expected_RegWrite, expected_MemRead, expected_MemWrite, expected_Branch;
-	logic [1:0] expected_ALUOp;
+	logic expected_Reg2Loc, expected_MemtoReg, expected_RegWrite, expected_MemRead, expected_MemWrite, expected_Branch, expected_ERet;
+	logic [1:0] expected_ALUOp, expected_ALUSrc;
+	logic [3:0] expected_EStatus;
 
 
-	logic [19:0] inputs_and_expected_outputs [0:6] = '{
-		{11'b111_1100_0010, 1'b0, 1'b1, 1'b1, 1'b1, 1'b1, 1'b0, 1'b0, 2'b00}, // LDUR
-		{11'b111_1100_0000, 1'b1, 1'b1, 1'b0, 1'b0, 1'b0, 1'b1, 1'b0, 2'b00}, // STUR
-		{11'b101_1010_0000, 1'b1, 1'b0, 1'b0, 1'b0, 1'b0, 1'b0, 1'b1, 2'b01}, // CBZ
-		{11'b100_0101_1000, 1'b0, 1'b0, 1'b0, 1'b1, 1'b0, 1'b0, 1'b0, 2'b10}, // ADD
-		{11'b110_0101_1000, 1'b0, 1'b0, 1'b0, 1'b1, 1'b0, 1'b0, 1'b0, 2'b10}, // SUB
-		{11'b100_0101_0000, 1'b0, 1'b0, 1'b0, 1'b1, 1'b0, 1'b0, 1'b0, 2'b10}, // AND
-		{11'b101_0101_0000, 1'b0, 1'b0, 1'b0, 1'b1, 1'b0, 1'b0, 1'b0, 2'b10}  // ORR
+	logic [25:0] inputs_and_expected_outputs [0:6] = '{
+	//   opcode             r2l   ALUs   mtr   rw    mr    mw    br    Eret  ALUo   EStatus
+		{11'b111_1100_0010, 1'b0, 2'b01, 1'b1, 1'b1, 1'b1, 1'b0, 1'b0, 1'b0, 2'b00, 4'b0000}, // LDUR
+		{11'b111_1100_0000, 1'b1, 2'b01, 1'b0, 1'b0, 1'b0, 1'b1, 1'b0, 1'b0, 2'b00, 4'b0000}, // STUR
+		{11'b101_1010_0000, 1'b1, 2'b00, 1'b0, 1'b0, 1'b0, 1'b0, 1'b1, 1'b0, 2'b01, 4'b0000}, // CBZ
+		{11'b100_0101_1000, 1'b0, 2'b00, 1'b0, 1'b1, 1'b0, 1'b0, 1'b0, 1'b0, 2'b10, 4'b0000}, // ADD
+		{11'b110_0101_1000, 1'b0, 2'b00, 1'b0, 1'b1, 1'b0, 1'b0, 1'b0, 1'b0, 2'b10, 4'b0000}, // SUB
+		{11'b100_0101_0000, 1'b0, 2'b00, 1'b0, 1'b1, 1'b0, 1'b0, 1'b0, 1'b0, 2'b10, 4'b0000}, // AND
+		{11'b101_0101_0000, 1'b0, 2'b00, 1'b0, 1'b1, 1'b0, 1'b0, 1'b0, 1'b0, 2'b10, 4'b0000}  // ORR
 	};
 
 	int errors;
@@ -31,13 +35,13 @@ module maindec_tb();
 
 		for (int i = 0; i < 7; ++i) begin
 			#1ns;
-			{opcode, expected_Reg2Loc, expected_ALUSrc, expected_MemtoReg, expected_RegWrite, expected_MemRead, expected_MemWrite, expected_Branch, expected_ALUOp} =
+			{opcode, expected_Reg2Loc, expected_ALUSrc, expected_MemtoReg, expected_RegWrite, expected_MemRead, expected_MemWrite, expected_Branch, expected_ERet, expected_ALUOp, expected_EStatus} =
 				inputs_and_expected_outputs[i];
 			
 			#1ns;
 
-			if ({Reg2Loc, ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, ALUOp}
-					!= {expected_Reg2Loc, expected_ALUSrc, expected_MemtoReg, expected_RegWrite, expected_MemRead, expected_MemWrite, expected_Branch, expected_ALUOp}
+			if ({Reg2Loc, ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, ERet, ALUOp, EStatus}
+					!= {expected_Reg2Loc, expected_ALUSrc, expected_MemtoReg, expected_RegWrite, expected_MemRead, expected_MemWrite, expected_Branch, expected_ERet, expected_ALUOp, expected_EStatus}
 			) begin
 				errors++;
 				$display("ERROR: i = %d", i);
